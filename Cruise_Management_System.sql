9ef5d5bb3832afa3ad8804fa01dc55497501f844
dong.george03@gmail.com <gdong37@gatech.edu>
2024-06-18 11:11:22 -0400

    Created general information for attributes as well as tables for routing entities

--- Cruise_Management_System.sql
+++ Cruise_Management_System.sql
@@ -9,11 +9,11 @@
 -- Directions:
 -- Please follow all instructions for Phase II as listed on Canvas.
 -- Fill in the team number and names and GT usernames for all members above.
--- Create Table statements must be manually written, not taken from an SQL Dump
-file.
+-- Create Table statements must be manually written, not taken from an SQL Dump file.
 -- This file must run without error for credit.
 /* This is a standard preamble for most of our scripts. The intent is to establish
 a consistent environment for the database behavior. */
+
 set global transaction isolation level serializable;
 set global SQL_MODE = 'ANSI,TRADITIONAL';
 set names utf8mb4;
@@ -22,6 +22,7 @@ set @thisDatabase = 'cruise_tracking';
 drop database if exists cruise_tracking;
 create database if not exists cruise_tracking;
 use cruise_tracking;
+
 -- Define the database structures
 /* You must enter your tables definitions, along with your primary, unique and
 foreign key
@@ -30,3 +31,45 @@ order that
 works for you. When executed, your statements must create a functional database
 that contains
 all of the data, and supports as many of the constraints as reasonably possible. */
+
+/* GENERAL INFO
+identifying attributes - 50 or fewer alphanumeric characters
+dates - yyyy-mm-dd
+first / lastnames - 100 or fewer characters
+general attributes - 100 or fewer characters (unless specified)
+data specifications CAN change*/
+
+-- Tables used for the routing of cruises
+-- Includes the entities: route, leg, port
+create table port(
+    portID varchar(50) not null,
+    portName varchar(100),
+    portCity varchar(100),
+    portState varchar(100),
+    portCountry varchar(100),
+    primary key (portID)
+);
+
+create table leg(
+    legID varchar(50) not null,
+    distance int,
+    arrivalPort varchar(50) not null,
+    departurePort varchar(50) not null,
+    primary key (legID),
+    foreign key (arrivalPort) references port(portID),
+    foreign key (departurePort) references port(portID)
+);
+
+create table route(
+    routeID varchar(50) not null,
+    primary key (routeID)
+);
+
+create table contains(
+    routeID varchar(50) not null,
+    legSequence int not null,
+    legID varchar(50) not null,
+    foreign key (routeID) references route(routeID),
+    foreign key (legID) references leg(legID),
+    primary key (routeID, legSequence)
+);
